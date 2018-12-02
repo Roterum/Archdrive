1fba760181c60142fc63f05b0391ef0b86a4920c
TheEquah <theequah@gmail.com>
2018-12-02 21:30:05 +0100

    User added to sudo group.

--- archlinux-install-setup.sh
+++ archlinux-install-setup.sh
@@ -5,15 +5,12 @@ source /root/archlinux-install-variables.sh
 # [>] User Configuration
 
 # [i] Create new user for roterum.
-useradd -m $USERNAME
+useradd -m -G sudo $USERNAME
 
 # [i] Create user directory on drive.
 mkdir /drive/$USERNAME
 printf $"# [i] Archdrive - Mount drive folder.\n" >> /home/$USERNAME/.bash_login
 printf $"ln -s /home/$USERNAME/Drive /drive/$USERNAME\n" >> /home/$USERNAME/.bash_login
-
-# [i] Add user to sudoers.
-gpasswd -a $USERNAME sudo
 
 # [i] Set user password.
 echo $USERNAME:$PASSWD_USER | chpasswd

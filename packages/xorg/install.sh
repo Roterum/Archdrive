f6ea49574ca1efa7e2bb78e1e7cba795d58781ab
TheEquah <theequah@gmail.com>
2018-12-02 17:22:40 +0100

    Package install feature fixes.

--- packages/xorg/install.sh
+++ packages/xorg/install.sh
@@ -6,3 +6,12 @@
 
 # [i] Download X packages.
 pacman -Sy --noconfirm xorg-server xorg-xinit
+
+
+# [>] Setup
+
+
+# [i] Overwrite global xinitserverrc.
+
+printf $"#!/bin/sh" > /etc/X11/xinit/xserverrc
+printf $"exec /usr/bin/Xorg -nolisten tcp "\$\@" vt\$XDG_VTNR" > /etc/X11/xinit/xserverrc
